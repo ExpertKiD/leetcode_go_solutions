cff5dc6f2628da05de61688b39358a9757fad0e7
Suman Adhikari <expertxkid@gmail.com>
2022-01-20 14:10:29 +0545

    feat: remove main from problem 2

--- problems/problem_2.go
+++ problems/problem_2.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 // Link To Problem: https://leetcode.com/problems/add-two-numbers/
 
 // ListNode Definition for singly-linked list.
@@ -13,104 +9,61 @@ type ListNode struct {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var l1Arr []int
-	var l2Arr []int
 
-	var temp = l1
+	var carry = 0
+	var sum = 0
 
-	// convert l1 and l2 to array
-	for temp != nil {
-		l1Arr = append(l1Arr, temp.Val)
-		temp = temp.Next
-	}
-
-	temp = l2
-
-	for temp != nil {
-		l2Arr = append(l2Arr, temp.Val)
-		temp = temp.Next
-	}
-
-	fmt.Printf("%v\n", l1Arr)
-	fmt.Printf("%v\n", l2Arr)
-
-	num1, num2 := 0, 0
-
-	// convert arrays now to integer
-	for i := len(l1Arr) - 1; i >= 0; i-- {
-		num1 = num1*10 + l1Arr[i]
-	}
-
-	for i := len(l2Arr) - 1; i >= 0; i-- {
-		num2 = num2*10 + l2Arr[i]
-	}
-
-	// get the sum
-	var sum = num1 + num2
-
-	print(sum, "\n")
-
-	// convert sum to array, then to listnode and return
-	var tempNumber = sum
-	var sumArr []int
-
-	for tempNumber != 0 {
-		sumArr = append(sumArr, tempNumber%10)
-		tempNumber /= 10
-	}
-
-	fmt.Printf("%v\n", sumArr)
+	temp1 := l1
+	temp2 := l2
 
 	var l3 *ListNode = &ListNode{
 		Val:  0,
 		Next: nil,
 	}
 
-	tempNode := l3
+	var temp3 = l3
 
-	for i := 0; i < len(sumArr); i++ {
-		tempNode.Val = sumArr[i]
-		tempNode.Next = &ListNode{
-			Val:  0,
-			Next: nil,
+	for temp1 != nil || temp2 != nil {
+		var val1 = 0
+		var val2 = 0
+
+		if temp1 == nil {
+			val1 = 0
+		} else {
+			val1 = temp1.Val
 		}
 
-		if i == len(sumArr)-1 {
-			tempNode.Next = nil
+		if temp2 == nil {
+			val2 = 0
+		} else {
+			val2 = temp2.Val
 		}
 
-		tempNode = tempNode.Next
+		sum = val1 + val2 + carry
+
+		if sum >= 10 {
+			carry = sum / 10
+			sum = sum % 10
+		} else {
+			carry = 0
+		}
+
+		temp3.Val = sum
+
+		if temp1.Next == nil && temp2.Next == nil {
+			temp3.Next = nil
+		} else {
+			temp3.Next = &ListNode{
+				Val:  0,
+				Next: nil,
+			}
+		}
+
+		temp3 = temp3.Next
+
+		temp1 = temp1.Next
+		temp2 = temp2.Next
 	}
 
 	return l3
-}
-
-func main() {
-
-	var l1 *ListNode = &ListNode{
-		Val: 2,
-		Next: &ListNode{
-			Val: 4,
-			Next: &ListNode{
-				Val:  3,
-				Next: nil,
-			},
-		},
-	}
-
-	var l2 *ListNode = &ListNode{
-		Val: 5,
-		Next: &ListNode{
-			Val: 6,
-			Next: &ListNode{
-				Val:  4,
-				Next: nil,
-			},
-		},
-	}
-
-	l3 := addTwoNumbers(l1,
-		l2)
-
-	println(l3)
 }
